4c411c48d08040aa5e331131fbcc17ee5b0a03b7
Leith Reardon <leithreardon@gmail.com>
2018-07-24 16:24:43 -0700

    refactor login code

---
+++ To vs Do/Supporting Files/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  To vs Do
+//
+//  Created by Leith Reardon on 7/24/18.
+//  Copyright © 2018 Leith Reardon. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct Segue {
+        static let toCreateUsername = "toCreateUsername"
+    }
+}
